4232a503eeed13489dc8ed16ee4d52831731f98c
Samer Albahra <salbahra@gmail.com>
2015-07-03 22:20:02 -0500

    Initial commit to prep the base for Node Webkit rewrite

---
+++ build.js
@@ -0,0 +1,82 @@
+// TODO: automate updating desktop-app/package.json version
+console.log( "Before running, make sure versions are updated in both package.json and desktop-app/package.json" );
+
+var NwBuilder = require( "node-webkit-builder" );
+var appPkg = require( "./desktop-app/package.json" );
+var appName = "OS-Updater";
+
+var nw = new NwBuilder( {
+  files: "desktop-app/**",
+  platforms: [ "osx32", "win32", "linux32" ],
+  version: "0.11.6",
+  appName: appName,
+  appVersion: appPkg.version,
+  winIco: "icons/win.ico",
+  macIcns: "icons/mac.icns",
+  buildType: "default",
+  mergeZip: false
+} );
+
+nw.on( "log", console.log );
+
+nw.build()
+  .then( function() {
+    console.log( "All apps have been built successfully!" );
+    createDMG();
+    createNW();
+  } )
+  .catch( function( error ) {
+    console.error( error );
+  } );
+
+// create the regular .nw file for updates
+function createNW() {
+  console.log( "Creating regular updater.nw for updates..." );
+  var fs = require( "fs" ),
+    archiver = require( "archiver" ),
+    archive = archiver( "zip" );
+
+  var output = fs.createWriteStream( "./build/" + appName + "/OS-Updater-" + appPkg.version + ".nw" );
+  output.on( "close", function() {
+    console.log( ( archive.pointer() / 1000000 ).toFixed( 2 ) + "mb compressed" );
+  } );
+
+  archive.pipe( output );
+  archive.bulk( [
+    { expand: true, cwd: "desktop-app", src: [ "**" ], dest: "." }
+  ] );
+  archive.finalize();
+}
+
+// create the mac DMG installer
+function createDMG() {
+  console.log( "Creating Mac OS X DMG..." );
+  var appdmg = require( "appdmg" );
+  var ee = appdmg( {
+    source: "./dmg.json",
+    target: "./build/" + appName + "/OS-Updater.dmg"
+  } );
+
+  ee.on( "progress", function( info ) {
+
+    // info.current is the current step
+    // info.total is the total number of steps
+    // info.type is on of 'step-begin', 'step-end'
+
+    // 'step-begin'
+    // info.title is the title of the current step
+
+    // 'step-end'
+    // info.status is one of 'ok', 'skip', 'fail'
+    console.log( "DMG step " + info.current + "/" + info.total );
+
+  } );
+
+  ee.on( "finish", function() {
+    console.log( "Mac OS X DMG successfully created" );
+  } );
+
+  ee.on( "error", function( err ) {
+    console.log( err );
+  } );
+}
