6ff952c76f3cd3a08d98071ed8fc562c1d5552ef
Samer Albahra <salbahra@gmail.com>
2015-07-04 15:47:50 -0500

    Add example child process spawning

---
+++ desktop-app/js/app.js
@@ -0,0 +1,11 @@
+var exec = require( "child_process" ).exec,
+    child;
+
+child = exec( "cat *.js bad_file | wc -l",
+  function (error, stdout, stderr) {
+    console.log( "stdout: " + stdout );
+    console.log( "stderr: " + stderr );
+    if (error !== null) {
+      console.log( "exec error: " + error );
+    }
+});
