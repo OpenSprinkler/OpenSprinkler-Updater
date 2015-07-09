425839bfd240e88a52424cd632350a2253e0215b
Samer Albahra <salbahra@gmail.com>
2015-07-09 18:21:24 -0500

    Remove non-working Github release module

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function( grunt ) {
 
 	// Load node-modules;
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
-	grunt.loadNpmTasks( "grunt-github-releaser" );
 
 	// Project configuration.
 	grunt.initConfig( {
@@ -12,23 +11,6 @@ module.exports = function( grunt ) {
 			main: [ "Gruntfile.js", "build.js", "desktop-app/js/*.js" ],
 			options: {
 				jshintrc: true
-			}
-		},
-
-		"github-release": {
-			options: {
-				repository: "salbahra/OpenSprinkler-FW-OpenSprinkler",
-				auth: {
-					user: "username",
-					password: process.env.GITHUB_TERMINAL
-				}
-			},
-			files: {
-				"dest": [	"OpenSprinkler-FW-Updater-win32.zip",
-							"OpenSprinkler-FW-Updater-linux32.tar.gz",
-							"OpenSprinkler-FW-Updater-linux64.tar.gz",
-							"OpenSprinkler-FW-Updater.dmg"
-				]
 			}
 		}
 
